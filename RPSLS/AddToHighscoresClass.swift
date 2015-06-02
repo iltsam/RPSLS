bb2ae30a859d23d3b7eb0893f0084e63c5f6c3ba
Sam Marschke <sam@Sams-Mac-mini.local>
2015-06-02 04:22:45 -0700

    Highscores

--- RPSLS/AddToHighscoresClass.swift
+++ RPSLS/AddToHighscoresClass.swift
@@ -12,14 +12,27 @@ import CoreData
 class AddToHighscoresClass: UIViewController {
     var moves: Int!
     // Retrieve the managedObjectCentext from AppDelegate
-    let managedObjectContext = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
+    let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    
+    
+    
+    
 
+    @IBOutlet weak var nameLabel: UITextField!
     @IBOutlet weak var movesLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         movesLabel.text = "\(moves)"
         // Do any additional setup after loading the view.
-        println(managedObjectContext)
+        let context = appDelegate.managedObjectContext!
+        println(context)
+        
+        // Create a new fetch request for Highscores
+        let fetchRequest = NSFetchRequest(entityName: "Highscores")
+        // Execute the fetch request
+        if let fetchResults = context.executeFetchRequest(fetchRequest, error: nil) as? [Highscores] {
+            println(fetchResults)
+        }
         
         
     }
@@ -30,12 +43,27 @@ class AddToHighscoresClass: UIViewController {
     }
     
     func add () {
-        let newItem = NSEntityDescription.insertNewObjectForEntityForName("Highscores", inManagedObjectContext: self.managedObjectContext!) as! Highscores
-        newItem.player_name = "Josh"
+        let context = appDelegate.managedObjectContext!
+        println(context)
+        let newItem = NSEntityDescription.insertNewObjectForEntityForName("Highscores", inManagedObjectContext: context) as! Highscores
+        newItem.player_name = nameLabel.text
         newItem.player_moves = moves
+        
+        appDelegate.saveContext()
+        // Create a new fetch request for Highscores
+        let fetchRequest = NSFetchRequest(entityName: "Highscores")
+        // Execute the fetch request
+        if let fetchResults = context.executeFetchRequest(fetchRequest, error: nil) as? [Highscores] {
+            println(fetchResults)
+        }
     }
     
 
+    @IBAction func addButtonClicked(sender: AnyObject) {
+        // Adds name and moves to highscore
+        add()
+        
+    }
     /*
     // MARK: - Navigation
 
